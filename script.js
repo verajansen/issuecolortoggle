f8b8178f8b4f8b132bfddb82cb6a9361964e773f
verajansen <95960869+verajansen@users.noreply.github.com>
2022-07-27 10:03:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const myBackGround = document.getElementsByClassName(myBackGround);
+
+myBackGround.classlist.toggle();
+
+/*const clickOn = document.getElementsByClassName(".btn-toggle-nav");
+const clickButton = document.getElementsByClassName(".nav-main-ul");
+
+clickButton.classlist.toggle("visible");
+clickButton.classlist.toggle("hidden");*/
+
+/*clickOn.addEventListener("click", function () {
+	clickButton.style.visibility = "visible";
+});
+console.log(clickOn);*/
